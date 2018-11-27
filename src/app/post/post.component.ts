b906f7221007a08c91e086d33c210dbc1fdf666a
Deepak vaishnav <deepakvaishnav8880@gmail.com>
2018-11-27 23:27:00 +0530

    single post

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../blog.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  root_url;
-  constructor(private ser:BlogService) { }
+  public id: string;
+  root_url; post1;
+  constructor(private ser:BlogService,private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.root_url = this.ser.root_url;
+    this.id = this.route.snapshot.paramMap.get('id');
+    console.log(this.id);
+    this.ser.getposts(this.id).subscribe(data=>console.log(this.post1 = data));
   }
 
 }
